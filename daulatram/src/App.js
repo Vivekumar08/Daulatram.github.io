c553aa1aebc3d6b18c32917e2a6dcdcc258cdf46
riyaku11 <riyaku217@gmail.com>
2022-06-10 13:40:13 +0530

    header

--- daulatram/src/App.js
+++ daulatram/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main_header'>
+        <div className='logo'>
+          <img className='logo' src="\images\logo.png" alt="logo" />
+        </div>
+        <div className='header1'>
+          <span className='college_name' ><strong>Daulat Ram College, University of Delhi</strong></span >
+          <div className='header2'>
+            Star College, <strong>Estb. 1960 </strong>
+          </div>
+          <div className='header3'>
+            NIRF RANK: 26 | NAAC GRADE 'A' |  INDIA TODAY RANK (SCIENCE:7 COMMERCE:13 HUMANITIES:15 ) | DBT STAR COLLEGE
+          </div>
+        </div>
+
+      </div>
+    </>
   );
 }
 
