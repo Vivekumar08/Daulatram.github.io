1a1c61c10e5b96ddffbf9761cc9c4b7e3a1a6813
riyaku11 <riyaku217@gmail.com>
2022-06-11 17:20:13 +0530

    bulletin

--- daulatram/src/App.js
+++ daulatram/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Bulletin from './Components/Bulletin';
 import Header from './Components/Header'
 
 function App() {
   return (
     <>
       <Header />
+      {/* <Bulletin/> */}
 
     </>
   );
