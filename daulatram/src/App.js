b66e7490213a0f87b293e334eab91af2497cd867
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-06-12 11:21:43 +0530

    navbar

--- daulatram/src/App.js
+++ daulatram/src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
 import Header from './Components/Header'
+// import Navbar from './Components/Navbar';
+import Navbar from './Components/Nav';
+import Footer from './Components/Footer';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
+        <Navbar />
+        <Footer/>
+      </Router>
 
     </>
   );
---
+++ daulatram/src/Components/Mylinks.jsx
@@ -0,0 +1,241 @@
+export const links = [
+  {
+    name: "About Us",
+    // submenu: true,
+    links: [
+      {
+        name: "Founder",
+      },
+      {
+        name: "College Profile Mission and Vision",
+      },
+      {
+        name: "Chairperson's Message",
+      },
+      {
+        name: "Principal's Message",
+      },
+      {
+        name: "Vice-Principal's Message",
+      },
+      {
+        name: "Administration",
+      },
+      {
+        name: "Annual Report",
+      },
+      {
+        name: "Gallery",
+      },
+    ],
+  },
+  {
+    name: "Academics",
+    links: [
+        {
+            name: 'Departsments',
+        },
+        {
+            name: 'Teacher-In-Charges',
+        },
+        {
+            name: "Undergraduate Courses",
+        },
+        {
+            name: "Centre for Foreign Languages(Add-On Courses)",
+        },
+        {
+            name: "Facilities",
+        },
+        {
+            name:"Resources",
+        },
+        {
+            name: "Training Programs",
+        },
+        {
+            name: "Academic Calendar",
+        },
+        {
+            name: "College Academic Calendar",
+        },
+    ],
+  },
+  {
+    name: "Research",
+    links:[
+        {
+            name: "facilities",
+        },
+        {
+            name: "Publication",
+        },
+        {
+            name: "Magazine",
+        },
+        {
+            name: "Newsletters",
+        },
+    ],
+  },
+  {
+    name:"Admission",
+    links:[
+        {
+            name:"Online Admissions"
+        },
+        {
+            name:"Admissions Bulletin 2022-23"
+        },
+        {
+            name:"Admission Committee"
+        },
+        {
+            name:"Fee Structure"
+        },
+        {
+            name:"GE Options 2022-23"
+        },
+        {
+            name:"Helpdesk"
+        },
+        {
+            name:"OBC/SC/ST/EWS Complaints"
+        },
+        {
+            name:"Anti Ragging Guidlines"
+        },
+        {
+            name:"FAQS For Admissions"
+        },
+    ]
+  },
+  {
+    name:"Student ZOne",
+    links:[
+        {
+            name:"Time Table & Academics Calendar"
+        },
+        {
+            name:"Attendance"
+        },
+        {
+            name:"Internal Assesments"
+        },
+        {
+            name:"Examination Form And Datesheet"
+        },
+        {
+            name:"Hostel"
+        },
+        {
+            name:"Forms"
+        },
+        {
+            name:"Fee Payment Portal"
+        },
+        {
+            name:"Scholarship"
+        },
+        {
+            name:"Student's Union"
+        },
+        {
+            name:"Placement Cell"
+        },
+        {
+            name:"Students Facilities"
+        },
+        {
+            name:'E-Rsources'
+        },
+        {
+            name:"ICC"
+        },
+        {
+            name:"Anti-Ragging Helplines"
+        },
+        {
+            name:"Equal Oppotunities Cell"
+        },
+        {
+            name:"Grievance Redressal"
+        },
+        {
+            name:"Complaints/Suggestions"
+        },
+    ]
+  },
+  {
+    name:"Staff Zone",
+    links:[
+        {
+            name:"Staff Council Committee"
+        },
+        {
+            name:"Roster"
+        },
+        {
+            name:"Seniority List"
+        },
+        {
+            name:"Forms"
+        },
+        {
+            name:"Professional Code of Ethics"
+        },
+        {
+            name:"Notices"
+        },
+        {
+            name:"Feedback"
+        },
+    ]
+  },
+  {
+    name:"Accreditation",
+    links:[
+        {
+            name:"NAAC/IQAC"
+        },
+        {
+            name:"NAAC/AQAR"
+        },
+        {
+            name:"NIRF"
+        },
+        {
+            name:"AISHE"
+        },
+        {
+            name:"DBT Star College"
+        },
+        {
+            name:"Other Rankings"
+        },
+    ]
+  },
+  {
+    name:"Useful Links",
+    links:[
+        {
+            name:"Covid Information & Resources"
+        },
+        {
+            name:"Employement Opportunities"
+        },
+        {
+            name:"ECA Calendar"
+        },
+        {
+            name:"E-Paathshala"
+        },
+        {
+            name:"Swayam Protal"
+        },
+        {
+            name:"UGC"
+        },
+    ]
+  }
+];
