f46fbc4b2f542f9a015b4f6d44ac83f9fe7cba7a
riyaku11 <riyaku217@gmail.com>
2022-06-12 18:18:39 +0530

    slideshow

--- daulatram/src/App.js
+++ daulatram/src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import Bulletin from './Components/Bulletin';
 import Header from './Components/Header'
+import Slider from './Components/Slider/Slider';
+
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Bulletin/> */}
+       <Bulletin/> 
+       {/* <Slideshow/>  */}
+       <Slider/>
 
     </>
   );
