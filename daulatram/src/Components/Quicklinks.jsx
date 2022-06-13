e7b6ed452e2b1274f6c0d44f7216ad582052a0b6
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-06-13 11:49:50 +0530

    quick link hover effect

--- daulatram/src/Components/Quicklinks.jsx
+++ daulatram/src/Components/Quicklinks.jsx
@@ -15,37 +15,39 @@ function Quicklinks() {
     <>
       <div className="quick mt-0 mb-0 bg-orange-200 h-auto pb-10">
         <div class="quick_links top-[170px] flex flex-row items-center justify-center text-center text-white font-bold ">
-          <span className="quick_content">
+          <span  className="quick_content">
             <strong>Quick Links</strong>
-          </span>
+          </span >
         </div> 
+        
         <div className="row ">
           <div className="col">
-            <span className="gallery_item gallery_item-1">
+            <a href="#"  className="gallery_item gallery_item-1  cursor-pointer">
+
               <img className="attendance" src={attendance} alt="" />
               <p>
                 <strong>Attendance</strong>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-2">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-2  cursor-pointer">
               <img className="calendar" src={calendar} alt="" />
               <p>
                 <b>Calendar</b>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-3">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-3  cursor-pointer">
               <img className="datesheet" src={datesheet} alt="" />
               <p>
                 <b>Datesheet</b>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-4">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-4  cursor-pointer">
               <img className="hostel" src={hostel} alt="" />
               <p>
                 <b>Hostel Facilities</b>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-5">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-5  cursor-pointer">
               <img
                 className="internal_assessment"
                 src={internal_assessment}
@@ -54,44 +56,42 @@ function Quicklinks() {
               <p>
                 <b>Internal Assessment</b>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-6">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-6  cursor-pointer">
               <img className="placement_cell" src={placement_cell} alt="" />
               <p>
                 <b>Placement Cell</b>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-7">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-7  cursor-pointer">
               <img className="scholarship" src={scholarship} alt="" />
               <p>
                 <b>Scholarship</b>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-8">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-8  cursor-pointer">
               <img
-                className="student_facilities"
-                src={student_facilities}
-                alt=""
-              />
+                className="student_facilities" src={student_facilities} alt="" />
               <p>
                 <b>Student Facilities</b>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-9">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-9  cursor-pointer">
               <img className="student_union" src={student_union} alt="" />
               <p>
                 <b>Student Union</b>
               </p>
-            </span>
-            <span className="gallery_item gallery_item-10">
+            </a >
+            <a href="#"  className="gallery_item gallery_item-10  cursor-pointer">
               <img className="time_table" src={time_table} alt="" />
               <p>
                 <b>Time Table</b>
               </p>
-            </span>
+            </a >
+          </div>
           </div>
         </div>
-      </div>
+      
     </>
   );
 }
