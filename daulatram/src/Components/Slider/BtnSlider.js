413b9d299a1a9861d67235f099d5ffd753f42da5
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-06-14 00:50:28 +0530

    slide

--- daulatram/src/Components/Slider/BtnSlider.js
+++ daulatram/src/Components/Slider/BtnSlider.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Slider.css";
-import leftArrow from "./icons/left-arrow.svg";
-import rightArrow from "./icons/right-arrow.svg";
+// import leftArrow from "./icons/left-arrow.svg";
+// import rightArrow from "./icons/right-arrow.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function BtnSlider({ direction, moveSlide }) {
   console.log(direction, moveSlide);
   return (
     <button
       onClick={moveSlide}
-      className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
+      className={direction === "next" ? " btn-slide next " : "btn-slide prev"}
     >
-      <img src={direction === "next" ? rightArrow : leftArrow} />
+      <FontAwesomeIcon icon={direction === "next" ? faArrowRight : faArrowLeft} className="text-2xl"/>
     </button>
   );
 }
