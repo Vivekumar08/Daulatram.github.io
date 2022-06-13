79eb5f6a42096d53ffbc07fde75a86f0d4761b4b
riyaku11 <riyaku217@gmail.com>
2022-06-13 10:35:03 +0530

    events

--- daulatram/src/App.js
+++ daulatram/src/App.js
@@ -4,10 +4,10 @@ import Quicklinks from "./Components/Quicklinks";
 import Bulletin from "./Components/Bulletin";
 
 import './App.css';
-<<<<<<< HEAD
-import Bulletin from './Components/Bulletin';
-import Header from './Components/Header'
+import "./Components/Events/Events.css"
+
 import Slider from './Components/Slider/Slider';
+import Events from "./Components/Events/Events";
 
 
 function App() {
@@ -15,39 +15,10 @@ function App() {
     <>
       <Header />
        <Bulletin/> 
-       {/* <Slideshow/>  */}
+       
        <Slider/>
-=======
-import Navbar from './Components/Nav';
-import Footer from './Components/Footer';
-
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from "react-router-dom"
-
-function App() {
-    return ( <
-        >
-        <
-        Router >
-        <
-        Header / >
-        <
-        Bulletin / >
-        <
-        Navbar / >
-        <
-        Quicklinks / >
-        <
-        Footer / >
-        <
-        /Router>
->>>>>>> 88016f64afa8a45dcaf524a73254dbc008ef2171
-
-        <
-        />
+{/* <Events/> */}
+        </>
     );
 }
 
---
+++ daulatram/src/Components/Events/Events.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Events.css"
+const Events = () => {
+  return (
+    <>
+    <div className='main_div1'>
+     <div className='small_box'>
+    <img src="" alt="IQAC Events" />
+    </div> 
+    
+      <div className='small_box'>
+    <img src="" alt="Vidya Vistar" />
+    </div>
+     <div className='small_box'>
+    <img src="" alt="Add On Courses" />
+    </div>
+     <div className='small_box'>
+    <img src="" alt="Student Activities" />
+    </div> 
+    </div>
+    </>
+  )
+}
+
+export default Events
