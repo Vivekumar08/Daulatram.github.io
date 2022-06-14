b4674fff61285e61a6edac0594d623fa35f64b68
riyaku11 <riyaku217@gmail.com>
2022-06-14 11:23:56 +0530

    notice

---
+++ daulatram/src/Components/Info.js
@@ -0,0 +1,25 @@
+const Info = [
+{
+id: 1,
+info : "Zebrafish Summer Internship 2022",
+link : "www.google.com"
+},
+{
+    id: 2,
+    info : "Zebrafish winter Internship 2022",
+    link : "www.google.com"
+ },
+{
+        id: 3,
+        info : "Zebrafish Summer Internship 2022",
+        link : "www.google.com"
+},
+{
+    id: 3,
+    info : "Zebrafish Summer Internship 2022",
+    link : "www.google.com"
+},
+
+];
+
+export default Info;
--- daulatram/src/Components/Information.jsx
+++ daulatram/src/Components/Information.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import Info from "./Info"; //first step
 
 function Information() {
+
+
+  
   return (
+
     <>
-      <div className="bg-blue-100 pb-4">
+        
+        const [Info, setInfo] = useState(Info); 
+    
+  <div className="bg-blue-100 pb-4">
         <div className="quick_links top-[170px] flex flex-row items-center justify-center text-center text-white font-bold  ">
           <span className="quick_content uppercase">
             <strong>Information Section</strong>
@@ -15,7 +23,16 @@ function Information() {
             <span className="bg-[#273E47] rounded-t-lg flex flex-row justify-center  text-white pb-4  px-4 ">
               <span className="mt-3">Notice for Student</span>
             </span>
-            <span className="ml-4 text-sm mb-4 mt-4">Daulat Ram College</span>
+            {Info.map((curElem) => {
+     const {id , info , link} = curElem;
+     return (
+      <>
+        <span className=" information flex flex-col ml-4 text-sm mb-4 mt-4">{info}</span>
+      </>
+     )
+     })}
+
+           
           </div>
           <div className=" bg-[#d9d9d9] w-[20em] rounded-lg text-lg font-semibold">
             <span className="bg-[#273E47] rounded-t-lg flex flex-row justify-center text-white pb-4  px-4 ">
@@ -31,6 +48,7 @@ function Information() {
           </div>
         </div>
       </div>
+     
     </>
   );
 }
