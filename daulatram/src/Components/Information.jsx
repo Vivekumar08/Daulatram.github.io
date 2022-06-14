2a2b242bc0a79929360f4076f810d7cd62deb6c1
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-06-14 10:41:43 +0530

    Changes

--- daulatram/src/Components/Information.jsx
+++ daulatram/src/Components/Information.jsx
@@ -15,19 +15,19 @@ function Information() {
             <span className="bg-[#273E47] rounded-t-lg flex flex-row justify-center  text-white pb-4  px-4 ">
               <span className="mt-3">Notice for Student</span>
             </span>
-            <span className="ml-4 text-sm mb-4 mt-4">PAISE BADHAO,VRNA CHHOD KE CHALE JAYEGE, </span><br /> <span className="ml-4 text-sm mb-4 mt-4">FIR 400000 KHARCH KRNE PD JAYEGE </span>
+            <span className="ml-4 text-sm mb-4 mt-4">Daulat Ram College</span>
           </div>
           <div className=" bg-[#d9d9d9] w-[20em] rounded-lg text-lg font-semibold">
             <span className="bg-[#273E47] rounded-t-lg flex flex-row justify-center text-white pb-4  px-4 ">
               <span className="mt-3">Notice for Staff</span>
             </span>
-            <span className="ml-4 mt-4 text-sm mb-4">PAISE BADHAO,VRNA CHHOD KE CHALE JAYEGE, </span><br /> <span className="ml-4 mt-4 text-sm mb-4">FIR 400000 KHARCH KRNE PD JAYEGE </span>
+            <span className="ml-4 mt-4 text-sm mb-4">Daulat Ram College</span>
           </div>
           <div className=" bg-[#d9d9d9] w-[20em] rounded-lg text-lg font-semibold">
             <span className="bg-[#273E47] rounded-t-lg flex flex-row justify-center text-white pb-4  px-4 ">
               <span className="mt-3">Notice for Public</span>
             </span>
-            <span className="ml-4 mt-4 text-sm mb-4">PAISE BADHAO,VRNA CHHOD KE CHALE JAYEGE, </span><br /> <span className="ml-4 mt-4 text-sm mb-4">FIR 400000 KHARCH KRNE PD JAYEGE </span>
+            <span className="ml-4 mt-4 text-sm mb-4">Daulat Ram College</span>
           </div>
         </div>
       </div>
