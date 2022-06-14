eafcb9db933288aa8b7cf84ec416be8e5e6023b8
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-06-14 11:46:59 +0530

    info

--- daulatram/src/Components/Info.js
+++ daulatram/src/Components/Info.js
@@ -1,25 +1,70 @@
-const Info = [
-{
-id: 1,
-info : "Zebrafish Summer Internship 2022",
-link : "www.google.com"
-},
-{
-    id: 2,
-    info : "Zebrafish winter Internship 2022",
-    link : "www.google.com"
- },
-{
+export const StudentInfo = [
+    {
+        id: 1,
+        info: "Zebrafish Summer Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
+        id: 2,
+        info: "Zebrafish winter Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
         id: 3,
-        info : "Zebrafish Summer Internship 2022",
-        link : "www.google.com"
-},
-{
-    id: 3,
-    info : "Zebrafish Summer Internship 2022",
-    link : "www.google.com"
-},
+        info: "Zebrafish Summer Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
+        id: 3,
+        info: "Zebrafish Summer Internship 2022",
+        link: "https://www.google.com"
+    },
+
+];
+export const StaffInfo = [
+    {
+        id: 1,
+        info: " Staff Zebrafish Summer Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
+        id: 2,
+        info: "Staff  Zebrafish winter Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
+        id: 3,
+        info: " Staff Zebrafish Summer Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
+        id: 3,
+        info: "Zebrafish Summer Staff  Internship 2022",
+        link: "https://www.google.com"
+    },
+
+];
+export const PublicInfo = [
+    {
+        id: 1,
+        info: "Public Zebrafish Summer Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
+        id: 2,
+        info: "Public Zebrafish winter Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
+        id: 3,
+        info: " Public Zebrafish Summer Internship 2022",
+        link: "https://www.google.com"
+    },
+    {
+        id: 3,
+        info: " Public Zebrafish Summer Internship 2022",
+        link: "https://www.ugc.ac.in/"
+    },
 
 ];
 
-export default Info;
