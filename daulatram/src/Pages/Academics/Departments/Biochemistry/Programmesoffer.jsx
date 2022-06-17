df2331215c5a0b1c235f9fe50eeaca8faf24134e
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-06-17 19:13:55 +0530

    changes

--- daulatram/src/Pages/Academics/Departments/Biochemistry/Programmesoffer.jsx
+++ daulatram/src/Pages/Academics/Departments/Biochemistry/Programmesoffer.jsx
@@ -20,17 +20,61 @@ function Programmesoffer() {
           </h2>
           <div className="flex flex-row justify-evenly mt-16">
          
-            <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+            <span className=" mr-2 bg-blue-900 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-800">
               <a href={ge}>
                 Biochemistry as GE Course
               </a>
             </span>
-            <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+            <span className=" mr-2 bg-blue-900 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-800">
               <a href={bsc}>
                 B.Sc.(Hons) Biochemistry
               </a>
             </span>
           </div>
+          <div className="p-3 grid grid-cols-4 mr-2 ml-2 mt-11">
+                <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500"><a href="https://drive.google.com/file/d/1GagyR2UNzjijUt-JLiLxulvspvfLdEZF/view?usp=sharing">
+                  Faculty
+                </a>
+                </span>
+                <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+                <a href="./biochem/programmesoffer">
+                  Programmes Offered
+                </a>
+                </span>
+                <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+                  <a href="./biochem/association">
+                  Association
+                </a></span>
+                <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+                <a href="https://drive.google.com/file/d/1GagyR2UNzjijUt-JLiLxulvspvfLdEZF/view?usp=sharing">
+                  Student's Achievements
+                </a>
+                </span>
+                <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+                <a href="https://drive.google.com/file/d/1GagyR2UNzjijUt-JLiLxulvspvfLdEZF/view?usp=sharing">
+                  Events
+                </a>
+                </span>
+
+            <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+                <a href="./biochem/laboratorystaff">
+                  Laboratory Staff
+                </a>
+                </span>
+
+                <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+                <a href="https://drive.google.com/file/d/1GagyR2UNzjijUt-JLiLxulvspvfLdEZF/view?usp=sharing">
+                  Publications / Awards
+                </a>
+                </span>
+
+                <span className=" mr-2 bg-blue-600 rounded-lg p-2 flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-500">
+                <a href="https://drive.google.com/file/d/1GagyR2UNzjijUt-JLiLxulvspvfLdEZF/view?usp=sharing">
+                  Research
+                </a>
+                </span>
+                
+              </div>
         </div>
       </div>
     </div>
