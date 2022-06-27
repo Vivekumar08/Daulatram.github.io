93645db337585cf0da505f05284dcfbeb853d7e0
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-06-27 16:07:21 +0530

    Societies

--- daulatram/src/Pages/Societies.jsx
+++ daulatram/src/Pages/Societies.jsx
@@ -1,46 +1,40 @@
-import React,{useState} from 'react'
-import "./Societies.css"
-import {SocietiesInfo} from "./SocietiesInfo"
+import React, { useState } from "react";
+import "./Societies.css";
+import { SocietiesInfo } from "./SocietiesInfo";
 const Societies = () => {
   const [Info, setSocietiesInfo] = useState(SocietiesInfo);
 
   return (
     <>
-<div className='main_head'>
-    <h1><span>Societies</span></h1>
-</div>
+      <div className="main_head">
+        <h1>
+          <span>Societies</span>
+        </h1>
+      </div>
 
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"/>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-{/* <div class="btn">
-  <a href="https://www.linkedin.com/in/florin-cornea-b5118057/" target="_blank">
-    <i class="fab fa-linkedin"></i>
-  </a>
-</div> */}
-{Info.map((currElem)=>{
-  const { id, info, img,link } = currElem;
-return(
-<div class="first hero">
-  <img class="hero-profile-img" src={img} alt=""/>
-  <div class="hero-description-bk"></div>
-  {/* <div class="hero-logo">
+      {Info.map((currElem) => {
+        const { id, info, img, link } = currElem;
+        return (
+          <div class="first hero">
+            <img class="hero-profile-img" src={img} alt="" />
+            <div class="hero-description-bk"></div>
+            {/* <div class="hero-logo">
     <img src="https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557291375.3948_Dy2yZu_n.jpg" alt=""/>
   </div> */}
-  <div class="hero-description">
-    <p>{info}</p>
-  </div>
-  {/* <div  class="hero-date">
+            <div class="hero-description">
+              <p>{info}</p>
+            </div>
+            {/* <div  class="hero-date">
     <p>20.02.2019</p>
   </div> */}
-  <div class="hero-btn">
-    <a href={link}>Learn More</a>
-  </div>
-</div>
-);
-})}
+            <a href={link}>
+              <div class="hero-btn">Learn More</div>
+            </a>
+          </div>
+        );
+      })}
 
-
-{/* <div class="second hero">
+      {/* <div class="second hero">
   <img class="hero-profile-img" src="http://www.pennedpeeps.com/wp-content/uploads/2020/03/DSC_0346-768x555.jpg" alt=""/>
   <div class="hero-description-bk"></div>
   <div class="hero-logo">
@@ -56,10 +50,8 @@ return(
     <a href="#">Learn More</a>
   </div>
 </div> */}
-
-    
     </>
-  )
-}
+  );
+};
 
-export default Societies
+export default Societies;
