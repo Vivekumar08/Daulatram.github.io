aaa58a7317976b1cef0d828884016c0dd05ee8aa
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-06-29 18:40:40 +0530

    changes

--- daulatram/src/Pages/Academics/Departments/Commerce/Com_labstaff.jsx
+++ daulatram/src/Pages/Academics/Departments/Commerce/Com_labstaff.jsx
@@ -13,49 +13,52 @@ function Com_labstaff() {
         <div className="w-[350px]">
           <Departments />
         </div>
-        <div>
-          <div className="w-[1100px]">
-            <h2 className="text-4xl uppercase font-bold mb-5 mt-[5%] flex flex-row justify-center items-center ">
-              Laboratory Staff
-            </h2>
+        
+        <div className="w-[1100px]">
+          <h2 className="text-4xl uppercase font-bold mb-5 mt-[5%] flex flex-row justify-center items-center ">
+            Laboratory Staff
+          </h2>
 
-            <div className="table_section">
-              <table className="mt-1 ml-5 mr-5">
-                <thead>
-                  <tr>
-                    <th>
-                      <strong>Name</strong>{" "}
-                    </th>
+          <div className="table_section ">
+            <table className="mt-1 ml-5 mr-5">
+              <thead>
+                <tr>
+                  <th>
+                    <strong>Name</strong>{" "}
+                  </th>
 
-                    <th>
-                      <strong>Designation</strong>
-                    </th>
+                  <th>
+                    <strong>Designation</strong>
+                  </th>
 
-                    <th>
-                      <strong>CV</strong>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Mr. Gaurav Verma</td>
+                  <th>
+                    <strong>CV</strong>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Mr. Gaurav Verma</td>
 
-                    <td>Commerce Lab Assistant</td>
-                    <td>
+                  <td>Commerce Lab Assistant</td>
+                  <td>
+                    {" "}
+                    <a href={GauravCV} target="_blank" rel="noreferrer">
                       {" "}
-                      <a href={GauravCV} target="_blank" rel="noreferrer">
-                        {" "}
-                        <button className="btn">Click Here</button>
-                      </a>{" "}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+                      <button className="btn">Click Here</button>
+                    </a>{" "}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
+      <div className=" flex  flex-col mr-16 mt-32">
+        <Commerce />
+      </div>
       </div>
     </div>
+
   );
 }
 
