072a298fccbcac097348c43ec9ea6ff2a7f7fbdf
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-06-30 23:56:39 +0530

    Research and facilities backend

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-dotenv.config({path:'./config.env'})
+dotenv.config({ path: './config.env' })
 require('./db/conn')
 
+app.use(bodyParser.urlencoded(
+  { extended: true }
+))
 
 app.use(cors());
 app.use(express.json());
+app.set("view engine", "ejs")
 
 require('./models/adminSchema');
 require('./models/Admission/helpdeskAdmission');
--- server/router/api.js
+++ server/router/api.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const path = require('path');
+const multer = require('multer');
 const router = express.Router();
-// const nodemailer = require("nodemailer");
 const bcrypt = require('bcryptjs');
 // const jwt = require("jsonwebtoken")
 const User = require('../models/adminSchema');
 const Adminssion = require('../models/Admission/onlineAdmission');
 const helpdesk = require('../models/Admission/helpdeskAdmission');
+const File = require("../models/Research/Research_fac_Schema");
+
+
+// SET STORAGE
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, '../daulatram/public/images/uploads');
+    },
+    filename(req, file, cb) {
+      cb(null, `${new Date().getTime()}_${file.originalname}`);
+      // cb(null, file.fieldname + '-' + Date.now());
+    }
+  }),
+  limits: {
+    // max file size 1MB = 1000000 bytes
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpeg|jpg|png|pdf|doc|docx|xlsx|xls)$/)) {
+      return cb(
+        new Error(
+          'only upload files with jpg, jpeg, png, pdf, doc, docx, xslx, xls format.'
+        )
+      );
+    }
+    cb(undefined, true);
+  }
+});
 
 
 
@@ -25,9 +55,9 @@ router.post('/NewAdmin', async (req, res) => {
       return res.status(400).json({ error: "Fill the complete form" });
     }
 
-    const hashedPassword = await bcrypt.hash(Password,salt)
+    const hashedPassword = await bcrypt.hash(Password, salt)
 
-    const user = new User({Username:Username,Password:hashedPassword});
+    const user = new User({ Username: Username, Password: hashedPassword });
     await user.save();
     console.log("Form filled Successfully")
     return res.status(200).json({ message: "Form filled Successfully " })
@@ -60,7 +90,7 @@ router.post('/AdminLogin', async (req, res) => {
   } catch (err) {
     console.log(err);
   }
-  
+
 })
 
 router.delete('/delete/:id', async (req, res) => {
@@ -81,7 +111,7 @@ router.post('/admission_online', async (req, res) => {
   if (!Link || !Caption) {
     return res.status(400).json({ error: "Fill the Admission Details Properly" })
   }
-  const user = new Adminssion(req.body );
+  const user = new Adminssion(req.body);
   await user.save();
   console.log("Details Saved Successfully")
   return res.status(200).json({ message: "Details Saved Successfully " })
@@ -103,7 +133,7 @@ router.post('/helpdesk_admission', async (req, res) => {
   if (!Link || !Caption) {
     return res.status(400).json({ error: "Fill the Helpdesk Details Properly" })
   }
-  const user = new helpdesk(req.body );
+  const user = new helpdesk(req.body);
   await user.save();
   console.log("Details Saved Successfully")
   return res.status(200).json({ message: "Details Saved Successfully " })
@@ -112,5 +142,66 @@ router.delete('/deleteHelpdesk/:id', async (req, res) => {
   const delete_user = await helpdesk.findOneAndDelete({ _id: req.params.id });
   res.status(200).json(delete_user + "User deleted")
 })
+
+// Research
+router.delete('/delete_research_fac/:id', async (req, res) => {
+  const delete_user = await File.findOneAndDelete({ _id: req.params.id });
+  res.status(200).json(delete_user + "User deleted")
+})
+
+router.get('/research', async (req, res) => {
+  try {
+    const files = await File.find({});
+    const sortedByCreationDate = files.sort(
+      (a, b) => b.createdAt - a.createdAt
+    );
+    res.send(sortedByCreationDate);
+  } catch (error) {
+    res.status(400).send('Error while getting list of files. Try again later.');
+  }
+});
+
+router.post(
+  '/research_upload',
+  upload.single('file'),
+  async (req, res) => {
+    try {
+      const { title, description } = req.body;
+      const { path, mimetype } = req.file;
+      console.log(title,
+        description,
+        // path,
+        // mimetype
+        )
+      const file = new File({
+        title,
+        description,
+        file_path: path,
+        file_mimetype: mimetype
+      });
+      await file.save();
+      res.send('file uploaded successfully.');
+    } catch (error) {
+      res.status(400).send('Error while uploading file. Try again later.');
+    }
+  },
+  (error, req, res, next) => {
+    if (error) {
+      res.status(402).send(error.message);
+    }
+  }
+);
+
+router.get('/research_download/:id', async (req, res) => {
+  try {
+    const file = await File.findById(req.params.id);
+    res.set({
+      'Content-Type': file.file_mimetype
+    });
+    res.sendFile(path.join(__dirname, '..', file.file_path));
+  } catch (error) {
+    res.status(400).send('Error while downloading file. Try again later.');
+  }
+});
 
 module.exports = router;
