aa7fc4dd9e5478f6061f55a3dfbd86d29ad9d095
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-07-01 20:24:05 +0530

    Active link improvement

--- daulatram/src/Components/DepartSIde/Biochemistry.jsx
+++ daulatram/src/Components/DepartSIde/Biochemistry.jsx
@@ -1,73 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
+import { NavLink } from "react-router-dom";
+import { biochems } from "./Depart_side_data";
 
 const Biochemistry = () => {
+  const [current,setCurrent] = useState(false);
+  const [data,setData] = useState(biochems);
+
   return (
     <div>
-      <a href="/academics/departments/biochem/">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          About The Deparment
+      {data.map((curElem)=>(
+        <NavLink to={curElem.link}  onClick={()=>setCurrent(!current)} className={({isActive})=>(isActive?" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white border-2 border-[#000080] ":" mr-2 bg-[#fff] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-black border-2 border-[#000080]  hover:bg-[#000080] hover:text-white")}>
+        <span >
+          {curElem.title}
         </span>
-      </a>
-      <a href="/academics/departments/biochem/faculty">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Faculty
-        </span>
-      </a>
-      <a href="/academics/departments/biochem/programmesoffer">
-        <span className="  bg-[#000080] rounded-lg p-2 flex w-[220px] flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Programmes Offered
-        </span>
-      </a>
-      <a href="/academics/departments/biochem/association">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Association
-        </span>
-      </a>
-
-      <a href="/academics/departments/biochem/studentsachieve">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Student's Achievements
-        </span>{" "}
-      </a>
-      <a href="/academics/departments/biochem/events">
-      <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-        Events
-      </span>
-      </a>
-
-      <a href="/academics/departments/biochem/laboratorystaff">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Laboratory Staff
-        </span>
-      </a>
-
-      <a href="/academics/departments/biochem/publications">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Publications
-        </span>
-      </a>
-
-      <a href="/academics/departments/biochem/awards">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Awards
-        </span>
-      </a>
-
-      <a href="/academics/departments/biochem/research">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Research
-        </span>
-      </a>
-      <a href="https://drive.google.com/file/d/1GagyR2UNzjijUt-JLiLxulvspvfLdEZF/view?usp=sharing">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Time Table
-        </span>
-      </a>
-      <a href="https://drive.google.com/file/d/1GagyR2UNzjijUt-JLiLxulvspvfLdEZF/view?usp=sharing">
-        <span className=" mr-2 bg-[#000080] rounded-lg p-2 w-[220px] flex flex-row justify-center mb-2 mt-2 text-white hover:bg-blue-700">
-          Photo Gallery
-        </span>
-      </a>
+      </NavLink>
+        ))}
     </div>
   );
 };
