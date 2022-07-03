04d360b8f41e58cc41ce1d77fe3151351b8dd872
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-07-03 21:36:05 +0530

    Economics

--- daulatram/src/Pages/Academics/Departments/Economics/Eco_programoffered.jsx
+++ daulatram/src/Pages/Academics/Departments/Economics/Eco_programoffered.jsx
@@ -12,66 +12,72 @@ function Eco_programoffered() {
         <Ecobanner />
       </div>
       <div className="flex flex-row">
-        <div className="w-[350px]">
-          <Departments />
+        <div className=" flex  flex-col ml-2 mt-12">
+          <Economics />
         </div>
-        <div>
-          <div className="w-[1100px]">
-            <h2 className="text-4xl uppercase font-bold mb-5 mt-[5%] flex flex-row justify-center items-center ">
-              Program Offered
-            </h2>
-          </div>
+        <div className="w-full mr-16">
+          <h2 className="text-4xl uppercase font-bold mb-5 mt-[5%] flex flex-row justify-center items-center ">
+            Program Offered
+          </h2>
+
           <div className="flex flex-row">
             <div class="wrapper2 mt-5 mb-5">
-              <div class="card1">
+              <div class="card2">
                 <div className="">
-                  <span className=" text-2xl ml-8 font-bold">
+                  <span className=" text-2xl ml-2 font-bold">
                     BA (Hons) Economics
                   </span>
                 </div>
                 <div class="info">
-                  <h1 className="font-bold">Description</h1>
-                  <p>Syllabus</p>
+                  <h1 className="font-bold flex justify-center">Description</h1>
+                  <p className="flex justify-center">Syllabus</p>
                   <br />
 
                   <a href={BAh}>
-                    <button>View</button>
+                    <button className="w-full">View</button>
                   </a>
+                  <br />
+                  <br />
+                  <br />
                 </div>
               </div>
-              <div class="card1">
-                <span className=" text-2xl ml-8 font-bold">BA Program</span>
+              <div class="card2">
+                <span className=" text-2xl ml-12 font-bold">BA Program</span>
                 <div class="info">
-                  <h1 className="font-bold ">Description</h1>
-                  <p>Syllabus</p>
-<br />
+                  <h1 className="font-bold  flex justify-center ">
+                    Description
+                  </h1>
+                  <p className="flex justify-center">Syllabus</p>
+                  <br />
                   <a href={BAprog}>
-                    <button>View</button>
+                    <button className="w-full">View</button>
                   </a>
-
+                  <br />
+                  <br />
+                  <br />
                 </div>
               </div>
-              <div class="card1">
+              <div class="card2">
                 <div className="flex flex-col">
                   <div>
-                    <span className=" text-2xl ml-1 font-bold ">
+                    <span className=" text-2xl ml-7 font-bold ">
                       Program Offered
                       <br />
                     </span>
                   </div>
                 </div>
                 <div class="info">
-                  <h1 className="font-bold">Description</h1>
+                  <h1 className="font-bold flex justify-center">Description</h1>
                   <p>Structure</p>
                   <br />
                   <a href={progoffer}>
-                    <button>View</button>
+                    <button className="w-full">View</button>
                   </a>
+                  <br />
+                  <br />
+                  <br />
                 </div>
               </div>
-            </div>
-            <div className=" flex  flex-col  mt-10">
-              <Economics />
             </div>
           </div>
         </div>
