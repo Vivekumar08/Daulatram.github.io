0f3b9f3c30fcfed6cf095e9979a471b369a5672f
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-07-07 22:19:17 +0530

    Political complete

---
+++ daulatram/src/Pages/Academics/Departments/Political_Science/event_info.js
@@ -0,0 +1,6 @@
+export const Pol_Sci_fac_info = [{
+    id: 500,
+    event_name: "Political Science Events",
+    img: "/images/ImgPages/Political_Science/event_pol_sci.jpeg",
+    doc: "/images/ImgPages/Political_Science/Events_polsci.pdf",
+}, ]
