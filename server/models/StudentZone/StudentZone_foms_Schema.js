46f19186dc97a95933356e20659ea0b5bd560308
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-07-08 09:25:38 +0530

    backend student form

---
+++ server/models/StudentZone/StudentZone_foms_Schema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const StudentZone_foms_Schema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    link: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    file_path: {
+      type: String,
+      required: true
+    },
+    file_mimetype: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Student_forms = mongoose.model('Student_forms', StudentZone_foms_Schema);
+
+module.exports = Student_forms;
--- server/router/api.js
+++ server/router/api.js
@@ -25,6 +25,7 @@ const C_Acad_Cal = require('../models/Academics/C_Acad_Cal_Schema')
 const Teacher = require('../models/Academics/Teacher_Schema')
 const Feedback = require('../models/StaffZone/Feedback_Form_Schema')
 const Roster = require('../models/StaffZone/Roster_Schema')
+const Student_forms = require('../models/StudentZone/StudentZone_foms_Schema')
 
 const unlinkAsync = promisify(fs.unlink)
 
@@ -460,6 +461,43 @@ router.post(
       const { title, link } = req.body;
       const { path, mimetype } = req.file;
       const file = new Training({
+        title,
+        link,
+        file_path: path,
+        file_mimetype: mimetype
+      });
+      await file.save();
+      res.send('file uploaded successfully.');
+    } catch (error) {
+      res.status(400).send('Error while uploading file. Try again later.');
+    }
+  },
+  (error, req, res, next) => {
+    if (error) {
+      res.status(402).send(error.message);
+    }
+  }
+);
+// Student-Zone Forms
+
+router.get('/StudentZone_forms', async (req, res,) => {
+  const details = await Student_forms.find()
+  res.status(200).json(details)
+});
+router.delete('/delete_StudentZone_forms/:id', async (req, res) => {
+  const delete_user = await Student_forms.findOneAndDelete({ _id: req.params.id });
+  await unlinkAsync(delete_user.file_path)
+  res.status(200).json(delete_user + "User deleted")
+})
+
+router.post(
+  '/StudentZone_forms_add',
+  upload.single('file'),
+  async (req, res) => {
+    try {
+      const { title, link } = req.body;
+      const { path, mimetype } = req.file;
+      const file = new Student_forms({
         title,
         link,
         file_path: path,
