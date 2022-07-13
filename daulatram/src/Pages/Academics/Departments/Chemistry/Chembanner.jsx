5858029923adb2153d8e4c34ab4add7dc0c7557c
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-07-13 14:22:11 +0530

    Banner

--- daulatram/src/Pages/Academics/Departments/Chemistry/Chembanner.jsx
+++ daulatram/src/Pages/Academics/Departments/Chemistry/Chembanner.jsx
@@ -13,13 +13,12 @@ function DepartBanner() {
       >
         <div className="name">
           <div className="flex flex-row justify-center">
-
-          <span className="  text-[#fff] text-6xl  mt-12 bg-[#000080] p-5 pl-16 rounded-md  w-[25%]">
-            Chemistry
-          </span>
+            <p className="  text-[#fff] text-6xl shadow-lg  mt-12 font-bold  p-5 flex justify-center w-full rounded-md  ">
+              Chemistry
+            </p>
           </div>
           <div className=" pl-5 text-xl text-[#000080] bg-gray-400 pt-3 pb-3 mt-28">
-          <Link to={"/"}>
+            <Link to={"/"}>
               <span className="ml-5">Home</span>
             </Link>
             <span className="ml-5">Academics</span>
