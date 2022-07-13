9b4c30cf9eba46735c1ddc2e8990368ecabf45ca
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-07-13 12:50:32 +0530

    Pol Science

---
+++ server/models/Academics/Departments/Political_Science/Political_Science_Faculty_Schema.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const Political_Science_Faculty_Schema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    filter: {
+        type: String,
+        required: true,
+    },
+    img_data: {
+
+        file_path: [{
+            file_path1: {
+                type: String,
+                default: null
+            },
+            file_mimetype1: {
+                type: String,
+                default: null
+            },
+        }],
+        pdf_path: [{
+            pdf_path1: {
+                type: String,
+                default: "../daulatram/public/images/uploads"
+            },
+            pdf_mimetype1: {
+                type: String,
+                default: 'application/pdf'
+            },
+            value: {
+                type: String,
+                default: null
+            },
+        }],
+    }
+}, {
+    timestamps: true
+});
+
+const Political_Science_Faculty = mongoose.model('Political_Science_Faculty', Political_Science_Faculty_Schema);
+
+module.exports = Political_Science_Faculty;
