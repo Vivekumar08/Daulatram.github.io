8b308ded2d11098c2979ec8ecc4c899d5968a517
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2022-07-27 22:40:43 +0530

    faculty

--- daulatram/src/Pages/Academics/Departments/Biochemistry/Publications.jsx
+++ daulatram/src/Pages/Academics/Departments/Biochemistry/Publications.jsx
@@ -1,29 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import DepartBanner from "../../../../Components/Banners/DepartBanner";
 import Biochemistry from "../../../../Components/DepartSIde/Biochemistry";
 import Departments from "../../../../Components/Sidebar/Departments";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 function Publications() {
+  const [visible, setVisible] = useState(false);
   return (
     <div className=" flex flex-col">
       <div className="">
         <DepartBanner />
       </div>
       <div className="flex flex-row">
-      <div className=" flex  flex-col mt-12 ml-2 ">
+      <div className="md:hidden">
+          {visible ? (
+            <>
+              <div className=" flex  flex-col mt-8 ml-2">
+                <FontAwesomeIcon
+                  icon={faClose}
+                  size="lg"
+                  onClick={() => setVisible(!visible)}
+                  className=" border-2  border-[#000080] mr-2 hover:text-black text-white  rounded-lg p-2 cursor-pointer hover:bg-white bg-[#000080]"
+                />
+                <Biochemistry />
+              </div>
+            </>
+          ) : (
+            <div className=" flex  flex-col mt-8 ml-2">
+              <FontAwesomeIcon
+                icon={faBars}
+                size="lg"
+                onClick={() => setVisible(!visible)}
+                className="text-black border-2 border-[#000080] mr-2 hover:text-white bg-[#fff] rounded-lg p-2 cursor-pointer hover:bg-[#000080]"
+              />
+            </div>
+          )}
+        </div>
+      <div className=" md:flex hidden md:flex-col mt-12 ml-2 ">
               <Biochemistry/>
 
               </div>
         <div>
           <div className="w-full ml-2">
-            <h2 className="text-4xl uppercase font-bold mb-5 mt-[5%] flex flex-row justify-center items-center ">
+            <h2 className="md:text-4xl text-lg uppercase font-bold mb-5 mt-[5%] flex flex-row justify-center items-center ">
               Publications
             </h2>
           </div>
           <div className="flex flex-row">
             <div>
               <div>
-                <ol className="list-decimal mt-2 ml-10 mr-20 text-justify mb-4">
+                <ol className="md:text-base text-sm list-decimal mt-2 ml-10 mr-20 text-justify mb-4">
                   <li>
                     Manisha Gupta, Babbar, Simran, Goel Anita, Gupta Radhika,
                     Dohare Neeraj And Nanda Sarita Pollution Controlling Ability
