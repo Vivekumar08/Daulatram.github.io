a55a7536645a331cab01d35b3dd41e93a9b157bf
Ishika414 <raiishika414@gmail.com>
2022-08-03 18:48:25 +0530

    space

--- daulatram/src/Components/Mylinks.jsx
+++ daulatram/src/Components/Mylinks.jsx
@@ -371,7 +371,7 @@ export const acade = [
         link: "/academics/UG_courses",
       },
       {
-        name: "Centre for Foreign Languages(Add-On Courses)",
+        name: "Centre for Foreign Languages (Add-On Courses)",
         link: "/academics/Add_on_courses",
       },
       {
