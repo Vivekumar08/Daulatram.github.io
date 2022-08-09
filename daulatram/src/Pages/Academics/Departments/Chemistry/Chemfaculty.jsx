e4fce52e02f39293d497fbc80670a51824c9da60
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-08-09 16:08:26 +0530

    superannuated

--- daulatram/src/Pages/Academics/Departments/Chemistry/Chemfaculty.jsx
+++ daulatram/src/Pages/Academics/Departments/Chemistry/Chemfaculty.jsx
@@ -59,12 +59,9 @@ const Chem_faculty = () => {
 
   const del = async (id) => {
     console.log(id);
-    const response = await fetch(
-      `/delete_chem_faculty/${id}`,
-      {
-        method: "POST",
-      }
-    );
+    const response = await fetch(`/delete_chem_faculty/${id}`, {
+      method: "POST",
+    });
     const data = await response.json();
     if (data || response.status === 200) {
       fetchdata();
@@ -123,6 +120,9 @@ const Chem_faculty = () => {
           );
           setCaption("");
           setLink("");
+          setImg("");
+          setPreviewSrcImg("");
+          setIsPreviewAvailableImg(false);
           setAuth(true);
           fetchdata();
         } else {
@@ -140,7 +140,7 @@ const Chem_faculty = () => {
     <div className=" flex flex-col">
       <Chembanner />
       <div className="flex flex-row">
-      <div className="md:hidden">
+        <div className="md:hidden">
           {visible ? (
             <>
               <div className=" flex  flex-col mt-8 ml-2">
@@ -198,22 +198,12 @@ const Chem_faculty = () => {
                 );
               })}
           </div>
-          {/* {data1 &&
-            data1.map((curElem) => {
-              const {  filter } = curElem;
-              // console.log(curElem);
-              return (
-                <>{filter ==="Supernnuated"&& */}
 
-                  <div className="  ">
-                    <h2 className="md:text-2xl text-lg uppercase font-bold m-1 ml-2 flex  items-center  ">
-                      Superannuated
-                    </h2>
-                  </div>
-                  {/* }
-                </>
-              );
-            })} */}
+          <div className="  ">
+            <h2 className="md:text-2xl text-lg uppercase font-bold m-1 ml-2 flex  items-center  ">
+              Superannuated / Former Faculty
+            </h2>
+          </div>
           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full mt-5 mb-5">
             {data1 &&
               data1.map((curElem) => {
@@ -287,7 +277,7 @@ const Chem_faculty = () => {
                       value="Supernnuated"
                       className="p-2 text-lg acctive:text-white  block px-4 py-2 "
                     >
-                      Superannuated
+                      Superannuated / Former Faculty
                     </option>
                   </select>
                 </div>
--- daulatram/src/Pages/Academics/Departments/English/Eng_faculty.jsx
+++ daulatram/src/Pages/Academics/Departments/English/Eng_faculty.jsx
@@ -57,12 +57,9 @@ const Eng_faculty = () => {
 
   const del = async (id) => {
     console.log(id);
-    const response = await fetch(
-      `/delete_eng_faculty/${id}`,
-      {
-        method: "POST",
-      }
-    );
+    const response = await fetch(`/delete_eng_faculty/${id}`, {
+      method: "POST",
+    });
     const data = await response.json();
     if (data || response.status === 200) {
       fetchdata();
@@ -119,6 +116,9 @@ const Eng_faculty = () => {
               },
             }
           );
+          setImg("");
+          setPreviewSrcImg("");
+          setIsPreviewAvailableImg(false);
           setCaption("");
           setLink("");
           setAuth(true);
@@ -138,7 +138,7 @@ const Eng_faculty = () => {
     <div className=" flex flex-col">
       <Engbanner />
       <div className="flex flex-row">
-      <div className="md:hidden">
+        <div className="md:hidden">
           {visible ? (
             <>
               <div className=" flex  flex-col mt-8 ml-2">
@@ -196,21 +196,11 @@ const Eng_faculty = () => {
                 );
               })}
           </div>
-          {/* {data1 &&
-            data1.map((curElem) => {
-              const { filter } = curElem;
-              return (
-                <>
-                  {filter === "Supernnuated" && ( */}
           <div className="  ">
             <h2 className="md:text-2xl text-lg uppercase font-bold m-1 ml-2 flex  items-center ">
-              Superannuated
+              Superannuated / Former Faculty
             </h2>
           </div>
-          {/* )}
-                </>
-              );
-            })} */}
           <div class="grid grid-cols-1 sm:grid-cols-2 ml-5 lg:grid-cols-3 xl:grid-cols-4 w-full mt-5 mb-5">
             {data1 &&
               data1.map((curElem) => {
@@ -284,7 +274,7 @@ const Eng_faculty = () => {
                       value="Supernnuated"
                       className="p-2 text-lg acctive:text-white  block px-4 py-2 "
                     >
-                      Superannuated
+                      Superannuated / Former Faculty
                     </option>
                   </select>
                 </div>
