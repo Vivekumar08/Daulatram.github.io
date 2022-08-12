643c24ca4631904cbbe5fafc69a72249d14865c5
Ishika414 <raiishika414@gmail.com>
2022-08-12 20:39:16 +0530

    ICC

---
+++ server/models/StudentZone/ICC_Schema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Icc_Schema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        link: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        file_path: {
+            type: String,
+            required: true
+        },
+        file_mimetype: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+const ICC = mongoose.model('ICC', Icc_Schema);
+module.exports =ICC;
