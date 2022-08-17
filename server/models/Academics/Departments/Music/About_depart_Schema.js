7c84a6b6137b329b7ef4ef0bac0b5dc2b0142be7
Ishika414 <raiishika414@gmail.com>
2022-08-17 11:16:39 +0530

    about music model

---
+++ server/models/Academics/Departments/Music/About_depart_Schema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const Music_About_depart_Schema = mongoose.Schema(
+  {
+    img_data: {
+      file_path: [
+        {
+          file_path1: {
+            type: String,
+            default: null
+          },
+          file_mimetype1: {
+            type: String,
+            default: null
+          },
+          value: {
+            type: String,
+            default: null
+          },
+        }
+      ],
+      para: [
+        {
+          para1: {
+            type: String,
+          },
+        }
+      ],
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Music_About_depart = mongoose.model('Music_About_depart', Music_About_depart_Schema);
+
+module.exports = Music_About_depart;
